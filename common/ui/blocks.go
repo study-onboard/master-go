21762faada2fd93b9e65594e9aa87a9eac0d49fa
Kut Zhang <kut@sanlea.cn>
2023-02-06 11:52:09 +0800

    add comment

--- common/ui/blocks.go
+++ common/ui/blocks.go
@@ -2,12 +2,14 @@ package ui
 
 import "fmt"
 
+// print end
 func PrintEnd() {
 	fmt.Println("------------------------------------------------------------")
 	fmt.Println()
 	fmt.Println()
 }
 
+// print title
 func PrintTitle(title string) {
 	fmt.Println(title)
 	fmt.Println("============================================================")
